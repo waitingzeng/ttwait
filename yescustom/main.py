6c5655bc97e3957d04afb054817a01000e8b2baf
waitingzeng <ttwait@gmail.com>
2013-02-26 10:17:02 +0800

    fixbug

--- yescustom/main.py
+++ yescustom/main.py
@@ -46,6 +46,14 @@ if DEBUG:
 else:
     my_domain = 'www.customdiydropshipping.com'
 
+
+INSANDBOX = True
+if INSANDBOX:
+    PAYPAL_RECEIVER_EMAIL = 'ttsell_1361842354_biz@gmail.com'
+else:
+    PAYPAL_RECEIVER_EMAIL = 'chobotracy@gmail.com'
+
+
 class ProxyHandler(BaseHandler):
     SUPPORTED_METHODS = ['GET', 'POST', 'CONNECT']
     cache = False
@@ -180,7 +188,7 @@ class StaticHandler(ProxyHandler):
     cache = True
     response_replaces = [(target_doamin, my_domain),
          ('Custom Drop', 'Custom DIY Drop'), ('Customdropshipping', ' Customdiydropshipping'),
-         ('UA-28350827-1', ''), ('february.cst@hotmail.com', 'chobotracy@gmail.com'), ('kathy.yescustom@gmail.com', ''),
+         ('UA-28350827-1', ''), ('february.cst@hotmail.com', PAYPAL_RECEIVER_EMAIL), ('kathy.yescustom@gmail.com', ''),
          ('order.yescustom@yahoo.com', 'yesdiycustom@live.com'),('yescustom', 'yesdiycustom'), ('23.53', '0.01')
          ]
 
@@ -409,6 +417,8 @@ class MyOrderDetail(MyHandler):
 
 
 class CartPayment(HtmlHandler):
+    if INSANDBOX:
+        response_replaces = HtmlHandler.response_replaces + [('paypal.com', 'sandbox.paypal.com')]
     @tornado.web.asynchronous
     def get(self, order_sn, *args, **kwargs):
         if self.current_user:
@@ -452,16 +462,136 @@ class CartDone(HtmlHandler):
         if self.request.headers['Origin'] != 'https://www.paypal.com':
             return HtmlHandler.get(self, order_sn, *args, **kwargs)
 
-        
-        #if self.request.headers['Referer'].lower().find('')
-        if self.get_argument('payment_status', 'None') == 'Completed':
-            order, create = UserOrder.objects.get_or_create(user=self.current_user, order_sn=order_sn)
+        self.redirect('/myorder-order/code/%s' % order_sn)
+
+        #return HtmlHandler.get(self, order_sn, *args, **kwargs)
+
+
+class IPNHandler(RequestHandler):
+    def verify_ipn(self, data):
+        # prepares provided data set to inform PayPal we wish to validate the response
+        data["cmd"] = "_notify-validate"
+        params = urllib.urlencode(data)
+
+        if INSANDBOX:
+            # sends the data and request to the PayPal Sandbox
+            paypal_url = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+        else:
+            paypal_url = 'https://www.paypal.com/cgi-bin/webscr'
+
+        req = urllib2.Request(paypal_url, params)
+        req.add_header("Content-type", "application/x-www-form-urlencoded")
+        # reads the response back from PayPal
+        response = urllib2.urlopen(req)
+        status = response.read()
+
+        # If not verified
+        if not status == "VERIFIED":
+            return False
+
+        # if not the correct receiver email
+        if not sandbox and data['txn_type'] != 'subscr_payment'
+        and not data["receiver_email"] == PAYPAL_RECEIVER_EMAIL:
+            self.log.info('Incorrect receiver_email')
+            self.log.info(data['receiver_email'])
+            return False
+
+        # if not the correct currency
+        if not sandbox and not data.get("mc_currency") == "USD":
+            self.log.info('Incorrect mc_currency')
+            return False
+
+        # otherwise...
+        return True
+
+    def subscr_signup(self, data):
+        # handle a 'Signup' IPN message
+        # you can create a User object, for example,
+        # or set a user's plan
+        pass
+
+    def subscr_payment(self, data):
+        # handle a 'Payment' IPN message
+        # this message gets sent when you receive a recurring payment
+        # you can re-set your user's plan here
+        if data['payment_status'] == 'Completed':
+            order_sn = data['invoice']
+            order = UserOrder.objects.get(order_sn=order_sn)
+            order.payid = data['txn_id']
             order.status = OrderStatus.paid
             order.save()
+        pass
 
-        self.redirect('/myorder-order/code/%s' % order_sn)
-        
-        #return HtmlHandler.get(self, order_sn, *args, **kwargs)
+    def subscr_modify(self, data):
+        # handle a 'Modify' IPN message
+        # the Subscribe button has an option to allow users to modify
+        # their subscription plan
+        # you can upgrade your user's plan here
+        pass
+
+    def subscr_eot(self, data):
+        # handle a 'End of Transaction' IPN message
+        # at the end of the subscription period, this message gets sent
+        # you can disable a user here
+        pass
+
+    def subscr_cancel(self, data):
+        # handle a 'Cancel' IPN message
+        # when a user cancels his subscription (either in his PayPal page or
+        # in your website), this message gets sent
+        # you can disable a user here
+        pass
+
+    def subscr_failed(self, data):
+        # handle a 'Failed' IPN message
+        # sometimes something goes wrong while the IPN is being sent
+        # you can log the error here
+        pass
+
+    def post(self, sandbox=''):
+        data = {}
+
+        # the values in request.arguments are stored as single value lists
+        # we need to extract their string values
+        for arg in self.request.arguments:
+            data[arg] = self.request.arguments[arg][0]
+
+        # If there is no txn_id in the received arguments don't proceed
+        if data['txn_type'] == 'subscr_payment'
+        and not 'txn_id' in data:
+            self.log.info('IPN: No Parameters')
+            return
+
+        # Verify the data received with Paypal
+        if not self.request.host.split(':')[0] == 'localhost'
+        and not self.verify_ipn(data, sandbox):
+            self.log.info('IPN: Unable to verify')
+            return
+
+        self.log.info('IPN: Verified!')
+
+        # Now do something with the IPN data
+        if data['txn_type'] == 'subscr_signup':
+            # initial subscription
+            self.subscr_signup(data)
+        elif data['txn_type'] == 'subscr_payment':
+            # subscription renewed
+            self.subscr_payment(data)
+        elif data['txn_type'] == 'subscr_modify':
+            # subscription plan modified
+            self.subscr_modify(data)
+        elif data['txn_type'] == 'subscr_eot':
+            # subscription expired
+            self.subscr_eot(data)
+        elif data['txn_type'] == 'subscr_cancel':
+            # subscription canceled
+            self.subscr_cancel(data)
+        elif data['txn_type'] == 'subscr_failed':
+            # subscription failed
+            self.subscr_failed(data)
+
+        return
+
 
 
 class AboutUs(HtmlHandler):
@@ -490,6 +620,7 @@ handlers = [
     (r'/myorder-order/code/(.*)', MyOrderDetail),
     (r'/myorder-orders/.*', MyOrderOrders),
     (r'/my.*', MyHandler),
+    (r'//cart-respond_action/', IPNHandler),
     (r'/cart-done/cart/(.*)/', CartDone),
     (r'/cart-payment/cart/(.+)/.*', CartPayment),
     (r'/article-about/n/about_us', AboutUs),
